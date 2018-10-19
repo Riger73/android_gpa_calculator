d561362960da3b3b4a87465f6a03e311fc3900f8
Riger73 <timothyn@iinet.net.au>
2018-10-19 20:30:57 +1100

    Add fields

--- app/src/main/java/com/example/timot/gpatracker/MainActivity.java
+++ app/src/main/java/com/example/timot/gpatracker/MainActivity.java
@@ -41,4 +41,26 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void calculateGPAtotal() {
+        final double HD_GRADE_VAL = 48.0;
+        final double D_GRADE_VAL = 36.0;
+        final double C_GRADE_VAL = 24.0;
+        final double P_GRADE_VAL = 12.0;
+        final double N_GRADE_VAL = 0.0;
+        final double UNIT_LOAD_VAL = 12.0;
+
+        int HDs = Integer.parseInt(tfNumberOfHighDistinctions.getText());
+        int Ds = Integer.parseInt(tfNumberOfDistinctions.getText());
+        int Cs = Integer.parseInt(tfNumberOfCredits.getText());
+        int Ps = Integer.parseInt(tfNumberOfPasses.getText());
+        int Ns = Integer.parseInt(tfNumberOfFails.getText());
+
+        double GPA = ((HDs * HD_GRADE_VAL) + (Ds * D_GRADE_VAL) + (Cs * C_GRADE_VAL) +
+                (Ps * P_GRADE_VAL) + (Ns * N_GRADE_VAL)) / ((HDs * UNIT_LOAD_VAL) +
+                (Ds * UNIT_LOAD_VAL) + (Cs * UNIT_LOAD_VAL) + (Ps * UNIT_LOAD_VAL) +
+                (Ns * UNIT_LOAD_VAL));
+        mTextMessage.setText(String.format("%.2f", GPA));
+    }
+}
+
 }
